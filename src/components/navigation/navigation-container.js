1affd4894c9b90a302d54d8df4ea2337484766ca
jeffwgates <jeff.gates0507@gmail.com>
2020-06-29 21:11:59 -0600

    Added blog features for centered images

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { withRouter } from "react-router";
 import { NavLink } from "react-router-dom";
 
@@ -61,7 +63,11 @@ const NavigationComponent = (props) => {
 
       <div className="right-side">
         Jeffrey Gates
-        {props.loggedInStatus == 'LOGGED_IN' ? <a onClick={handleSignOut}>Sign Out</a> : null}
+        {props.loggedInStatus == 'LOGGED_IN' ? ( 
+          <a onClick={handleSignOut}>
+            <FontAwesomeIcon icon="sign-out-alt" />
+          </a>
+        ) : null}
       </div>
     </div>
   );
--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,14 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import BlogItem from "../blog/blog-item";
 
-export default function() {
-  return (
-    <div>
-      <h2>Blog</h2>
+class Blog extends Component {
+  constructor(props) {
+    super(props);
 
-      <div>
-        <Link to="/about-me">Read more about myself</Link>
-      </div>
-    </div>
-  );
+    this.state = {
+      blogItems: []
+    };
+
+    this.getBlogItems = this.getBlogItems.bind(this);
+    this.activateInfiniteScroll();
+  }
+
+
+  activateInfiniteScroll() {
+    window.onscroll = () => {
+      
+    }
+  }
+
+  getBlogItems() {
+    axios
+      .get("https://jgates5.devcamp.space/portfolio/portfolio_blogs", {
+        withCredentials: true
+      })
+      .then(response => {
+        this.setState({
+          blogItems: response.data.portfolio_blogs
+        });
+      })
+      .catch(error => {
+        console.log("getBlogItems error", error);
+      });
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
+  }
+
+  render() {
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />
+    });
+
+   
+
+
+    return (
+        <div className="blog-container">
+          <div className="content-container">{blogRecords}</div>
+        </div>
+    )
+  }
 }
+
+export default Blog;
